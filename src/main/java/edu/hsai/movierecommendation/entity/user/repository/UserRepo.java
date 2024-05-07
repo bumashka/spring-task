47ffd7b081ae9d28de1b1ebb532b8937a413ad01
bumashka <90649137+bumashka@users.noreply.github.com>
2024-05-07 21:37:54 +0300

    added base with postgres

--- src/main/java/edu/hsai/movierecommendation/entity/user/repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package edu.hsai.movierecommendation.entity.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-
-}
