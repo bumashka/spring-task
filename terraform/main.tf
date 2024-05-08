56918255084a034b117a09eec9a75511621afe1c
bumashka <90649137+bumashka@users.noreply.github.com>
2024-05-08 04:10:26 +0300

    changes in terraform/ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
 terraform {
-required_version = ">= 0.14.0"
+  required_version = ">= 0.14.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.48.0"
     }
   }
 }
